e99b680e4dd54e2513dd3bc2905b3fab0f9446fc
slevey087 <slevey087@gmail.com>
2019-01-05 01:56:30 -0800

    audit fix, globals script

--- lab/lab-globals.js
+++ lab/lab-globals.js
@@ -1,5 +1,5 @@
-NASH = require("./index.js")
+NASH = require("../index.js")
 Object.assign(global, NASH)
 Object.assign(global, NASH.Playables)
 
-NASH.Backend = require("./lib/engine.js").Backend
+NASH.Backend = require("../lib/engine.js").Backend
